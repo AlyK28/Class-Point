d8f354bbd00e36607aadff66dbaa21ad1e16e6ef
ali.karim@unimadesolutions.com <ali.karim@unimadesolutions.com>
2025-09-24 19:19:44 +0300

    Refactor ThisAddIn.cs and update .gitignore
    - Updated `ThisAddIn.cs` to include a new namespace and
      partial class definition. Removed unused using directives
      and added `ThisAddIn_Startup` method for handling startup
      events. Adjusted `InternalStartup` to connect event handlers.
    
    - Modified `.gitignore` to exclude Visual Studio temporary
      files, user-specific files, and build output directories.

--- ClassPoint-Interface/ThisAddIn.cs
+++ ClassPoint-Interface/ThisAddIn.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Linq;
-using PowerPoint = Microsoft.Office.Interop.PowerPoint;
-using Office = Microsoft.Office.Core;
-
-namespace ClassPoint_Interface
+﻿namespace ClassPoint_Interface
 {
     public partial class ThisAddIn
     {
@@ -29,7 +21,7 @@ namespace ClassPoint_Interface
             this.Startup += new System.EventHandler(ThisAddIn_Startup);
             this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
         }
-        
-        #endregion
+
+        #endregion`
     }
 }
